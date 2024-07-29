c25d7eb94f1faf75ab527241881fcd5a2f96768a
DELL <DELL@DESKTOP-KSJQAP9>
2024-07-29 18:54:13 +0100

    First Commit

---
+++ JobinProject/src/main/java/pageobjects/OrderSummary.java
@@ -0,0 +1,29 @@
+package pageobjects;
+
+import java.security.PublicKey;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import  actiondriver.Action;
+import  base.BaseClass;
+
+
+public class OrderSummary extends BaseClass {
+	
+	Action action= new Action();
+	
+	@FindBy(xpath="//span[contains(text(),'I confirm my order')]")
+	private WebElement confirmOrderBtn;
+	
+	public OrderSummary() {
+		PageFactory.initElements(getDriver(), this);
+	}
+
+	public OrderConfirmationPage clickOnconfirmOrderBtn() throws Throwable {
+		action.click(getDriver(), confirmOrderBtn);
+		return new OrderConfirmationPage();
+	}
+	
+}
